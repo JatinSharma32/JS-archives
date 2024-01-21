de95369a36fbda58422298f3fb5c10352fd97705
Jatin <jatinsharma9038@gmail.com>
2024-01-21 12:29:03 +0530

    microtask vs callback queue & WrapperObject

---
+++ util.js
@@ -0,0 +1,32 @@
+// let hello = () => {
+//     console.log("this1: ", this);
+// };
+// let hello2 = function () {
+//     console.log("this2: ", this);
+// };
+// let obj = {
+//     h1: () => {
+//         console.log("this1: ", this);
+//     },
+//     h2: function () {
+//         console.log("this2: ", this);
+//     },
+// };
+
+// // hello();
+// // hello2();
+
+// obj.h1();
+// obj.h2();
+
+// class hllo {
+//     fsfs = 0;
+//     hllo() {
+//         console.log("thisisit: ", this);
+//     }
+// }
+// let jfa = new hllo();
+// jfa.hllo();
+
+// function p() {
+// }
